b1b3ab181663c978675e4ac783890f39d79c27a4
Celmas <ilnursup@mail.ru>
2017-12-05 09:22:23 +0300

    Update Main.java

--- LineWithPoints/src/sample/Main.java
+++ LineWithPoints/src/sample/Main.java
@@ -15,15 +15,11 @@ import java.util.Scanner;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        final LineChart<Number,Number> lineChart = new LineChart<>(xAxis,yAxis);
         primaryStage.setTitle("Изи");
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.show();
 
-    }
-
-
-    public static void main(String[] args) {
         Line[] line = new Line[25];
         SortedListOfPoints list = new SortedListOfPoints();
         Scanner scanner = null;
@@ -44,11 +40,25 @@ public class Main extends Application {
             line[i].show();
             i++;
         }
+        XYChart.Series series[] = new XYChart.Series[i];
+        for(int j = 0; j < i; j++){
+            series[j] = new XYChart.Series();
+            for (int k = 0; k < line[j].points.length; k++) {
+                series[j].getData().add(new XYChart.Data(line[j].points[k].x, line[j].points[k].y));
+            }
+            lineChart.getData().add(series[j]);
+        }
 /*        list.show();
         System.out.println(countOfLines);*/
 
         primaryStage.setScene(new Scene(lineChart, 800, 600));
         primaryStage.show();
+
+    }
+
+
+    public static void main(String[] args) {
+
         launch(args);
 
     }
